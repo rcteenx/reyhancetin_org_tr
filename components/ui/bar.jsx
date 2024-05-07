6df481041c6132e2d48fbc7400000316f4b12b39
“Reyhan <rcteenx@gmail.com>
2024-05-07 16:10:49 +0300

    fix mobile bar padding

--- components/ui/bar.jsx
+++ components/ui/bar.jsx
@@ -3,13 +3,13 @@ export default function Bar({ info }) {
     <section className="bg-slate py-16 text-white">
       <div className=" container mx-auto">
         <h2 className="text-2xl md:text-4xl text-center">{info.h2}</h2>
-        <div className="grid grid-cols-2 lg:grid-cols-4 gap-10 lg:gap-0 lg:divide-x divide-slate-600 [&>*]:pl-14 mt-10 text-center sm:text-left">
-          {info.items.map((i) => (
-            <div>
+        <div className="grid grid-cols-1 lg:grid-cols-4 gap-4 lg:divide-x divide-slate-600 lg:[&>*]:pl-14 mt-10 sm:text-left">
+          {info.items.map((i, index) => (
+            <div key={index} className="text-center lg:text-left">
               <div className="text-lg md:text-2xl text-gray-400 ">
                 {i.title}
               </div>
-              <div className="text-2xl md:text-[2.5rem] text-white font-extrabold mt-2">
+              <div className="text-2xl md:text-[2.5rem] text-white font-extrabold lg:mt-2">
                 {i.info} +
               </div>
             </div>

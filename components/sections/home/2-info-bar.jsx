d50b67185408937716070e302ea27e35b91fc83c
“Reyhan <rcteenx@gmail.com>
2024-06-20 17:41:52 +0300

    update info bar grid

--- components/sections/home/2-info-bar.jsx
+++ components/sections/home/2-info-bar.jsx
@@ -4,13 +4,13 @@ export default function InfoBar({ info }) {
       <div className="container mx-auto text-center">
         <h2 className="my-0 text-lg md:text-3xl lg:text-5xl ">{info.h2}</h2>
         <p className="text-indigo-light-5">{info.desc}</p>
-        <div className="grid grid-cols-1 lg:grid-cols-4 gap-4 lg:divide-x divide-indigo-light-1 lg:[&>*]:pl-14 mt-4 lg:mt-8 md:text-left">
+        <div className="grid grid-cols-2 lg:grid-cols-4 gap-4 lg:divide-x divide-indigo-light-1 lg:[&>*]:pl-14 mt-4 lg:mt-8 md:text-left">
           {info.items.map((i, index) => (
             <div key={index} className="text-center lg:text-left">
               <div className="text-lg lg:text-2xl text-indigo-light-3 ">
                 {i.title}
               </div>
-              <div className="text-2xl lg:text-[2.5rem] text-white font-extrabold lg:mt-2">
+              <div className="text-4xl lg:text-6xl text-white font-extrabold lg:mt-2">
                 {i.info} +
               </div>
             </div>
